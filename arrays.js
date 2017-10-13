13c422a3131059e9cadffa5643902286aadb0558
David Brennan <ddbren@gmail.com>
2017-10-13 18:36:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array = [element, ...array]
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
