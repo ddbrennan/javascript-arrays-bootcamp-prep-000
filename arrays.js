6ac31c5ff0bbe5f5f80829bd55deaf50cd5b510b
David Brennan <ddbren@gmail.com>
2017-10-13 18:49:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,12 @@ function removeElementFromBeginningOfArray(array) {
   newArray = array.slice(1)
   return newArray
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  newArray = array.slice(0, array.length-1)
+  return newArray
+}
